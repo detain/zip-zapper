b535cb2b4972f65ff6cf1046473fe9f24c0cf1b0
Joe Huss <detain@interserver.net>
2017-03-21 11:34:07 +0000

    lots of updates finishign the parser
    git-svn-id: https://creation.interserver.net/svn/zip-zapper/trunk@13 aa6aac3b-29b0-49c2-8e19-a6c8bccb4a8e

--- parse.php
+++ parse.php
@@ -25,8 +25,63 @@ require(__DIR__ . '/../../../include/functions.inc.php');
 function_requirements('getcurlpage');
 $page = getcurlpage('https://en.wikipedia.org/wiki/Special:Export/List_of_postal_codes');
 $page = str_replace("\n\n", "\n", $page);
-preg_match_all("/^\|-\.*.^\| (?P<country>.*)$.^\| *(?P<since>.*)$.^\| *(?P<iso>.*)$.^\| *(?P<area>.*)$.^\| *(?P<street>.*)$.^\| (?P<notes>.*)$/msU", $page, $matches);
-print_r($matches);
+$lines = explode("\n", $page);
+$found = [];
+for($x = 0; $x < sizeof($lines); $x++) {
+	$line = $lines[$x];
+	if ((trim($line) == '|-' || trim($line) == '|-.') && substr($lines[$x + 1], 0, 1) != '!') {
+		$x++;
+		$country = preg_replace('/\| *\[\[Postal codes in [^\|]*\|(.*)\]\]/msU', '\1', $lines[$x]);
+		$x++;
+		$x++;
+		$iso = preg_replace('/\| *\[\[ISO 3166-[0-9]*:[A-Z]*\|(.*)\]\]/msU', '\1', $lines[$x]);
+		$x++;
+		$area = trim(substr($lines[$x], 1));
+		$x++;
+		$street = trim(substr($lines[$x], 1));
+		$x++;
+		$notes = trim(substr($lines[$x], 1));
+		$codes = [];
+		$area = explode(',', $area);
+		foreach ($area as $each_area)
+			if (trim($each_area) != '' && trim($each_area) != '- no codes -')
+				$codes[] = "'".trim($each_area)."'";
+		$street = explode(',', $street);
+		foreach ($street as $each_street)
+			if (trim($each_street) != '')
+				$codes[] = "'".trim($each_street)."'";
+		$found[] = $iso;
+		echo "		'$iso' => [" . str_replace(['N','A'], ['#', '@'], implode(", ", $codes)) . "]," . (sizeof($codes) == 0 ? '	' : '') . "		// $country" . (trim($notes) != '' ? ', Notes: '.$notes :  '') . "\n";
+	}
+}
+$db = $GLOBALS['tf']->db;
+$db->query("select * from country_t order by iso2;");
+while ($db->next_record(MYSQL_ASSOC)) {
+	if (!in_array($db->Record['iso2'], $found))
+		echo "		'{$db->Record['iso2']}' => [],			// {$db->Record['short_name']}\n";
+}
+exit;
+
+
+
+preg_match_all('/^\|-\.*.^\| \[\[Postal codes in [^\|]*\|(?P<country>[^$]*)\]\]$.^\| *(?P<since>[^$]*)$.^\| *\[\[ISO 3166-1:[A-Z]*\|(?P<iso>[^$]*)\]\]$.^\| *(?P<area>[^$]*)$.^\| *(?P<street>[^$]*)$.^\| (?P<notes>[^$]*)$/msU', $page, $matches);
+//print_r($matches);
+foreach ($matches['country'] as $idx => $country) {
+	$iso = $matches['country_iso'][$idx];
+	$area = $matches['area'][$idx];
+	$street = $matches['street'][$idx];
+	$notes = $matches['notes'][$idx];
+	$codes = [];
+	$area = explode(',', $area);
+	foreach ($area as $each_area)
+		if (trim($each_area) != '' && trim($each_area) != '- no codes -')
+			$codes[] = "'".trim($each_area)."'";
+	$street = explode(',', $street);
+	foreach ($street as $each_street)
+		if (trim($each_street) != '')
+			$codes[] = "'".trim($each_street)."'";
+	echo "		'$iso' => [" . implode(", ", $codes) . "]	// $country" . (trim($notes) != '' ? ', Notes: '.$notes :  '') . "\n";
+}
 //$page = getcurlpage('https://en.wikipedia.org/wiki/List_of_postal_codes');
 //function_requirements('xml2array');
 //$data = xml2array($page, 1, 'attribute');
@@ -39,7 +94,7 @@ $file = file_get_contents('zip.txt');
 $lines = explode("\n", $file);
 $zips = [];
 foreach ($lines as $line) {
-	list($country, $area_zip, $street_zip, $notes) = explode("\t", $line);
+	list($country, $street_zip, $street_zip, $notes) = explode("\t", $line);
 	if (!isset($zips($country))) {
 		$zips[$country] = [];
 	$parts = explode(',', $area_zip);
